609bff4c70e2748c48fdbf531cb4afb7abe02707
amaurybeautiful <96389435+amaurybeautiful@users.noreply.github.com>
2025-11-16 01:46:03 -0300

    Update vite.config.ts

--- vite.config.ts
+++ vite.config.ts
@@ -9,6 +9,11 @@ export default defineConfig(({ mode }) => {
         port: 3000,
         host: '0.0.0.0',
       },
+         preview: {
+        allowedHosts: ['site-zentro.dxq9e0.easypanel.host'],
+        host: true,
+        port: 3000,
+      },
       plugins: [react()],
       define: {
         'process.env.API_KEY': JSON.stringify(env.GEMINI_API_KEY),
