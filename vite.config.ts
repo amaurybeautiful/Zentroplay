b5d3c453a28e53009a49b7a9202303fb20af3bd0
amaurybeautiful <96389435+amaurybeautiful@users.noreply.github.com>
2025-11-16 01:59:03 -0300

    Update vite.config.ts

--- vite.config.ts
+++ vite.config.ts
@@ -10,7 +10,7 @@ export default defineConfig(({ mode }) => {
         host: '0.0.0.0',
       },
          preview: {
-        allowedHosts: ['site-zentro.dxq9e0.easypanel.host'],
+        allowedHosts: ['zentroplay.net'],
         host: true,
         port: 3000,
       },
